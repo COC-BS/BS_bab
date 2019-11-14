747d93f479c3caaf9ba2aa5ad3bb6185dee4b91f
Coray <cyrill.coray@students.fhnw.ch>
2019-11-14 19:41:04 +0100

    Initialize Project

---
+++ 99_Projektarbeit_COC_BRA/99_Projektarbeit_COC_BRA/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 14.11.2019 17:43:30
+ * Author : cyril
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
---
+++ 99_Projektarbeit_COC_BRA/99_Projektarbeit_COC_BRA/Projektarbeit_COC_BRA/Sketch.cpp
@@ -0,0 +1,32 @@
+﻿/*Begining of Auto generated code by Atmel studio */
+#include <Arduino.h>
+
+/*End of auto generated code by Atmel studio */
+
+
+//Beginning of Auto generated function prototypes by Atmel Studio
+//End of Auto generated function prototypes by Atmel Studio
+
+// Visual Micro is in vMicro>General>Tutorial Mode
+// 
+/*
+    Name:       Pruefung_2.ino
+    Created:	06.06.2019 08:11:55
+    Author:     DESKTOP-TH2GEUU\cyril
+*/
+
+
+// The setup() function runs once each time the micro-controller starts
+void setup()
+{
+	Serial.begin(9600);
+	
+}
+
+// Add the main program code into the continuous loop() function
+void loop()
+{
+	
+	
+	
+}
